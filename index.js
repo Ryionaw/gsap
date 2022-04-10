3c09ecee1c91a0a4b955a503c0dd9a65fe57d546
Iwan Darmawan <iwan.darmawan@orami.com>
2022-04-10 17:51:14 +0700

    try to resolve

--- index.js
+++ index.js
@@ -18,107 +18,109 @@ const tl = gsap
       end: "+=400%",
     },
   })
-  //  Make Video Full Large
-  .set(".videoSectionFull", {
-    scale: 1.8,
-    ease: "none",
-    yPercent: 5,
-  })
-  // Set Animation Gone
-  .set("#animation3", { xPercent: 350 })
-  .set("#animation2", { xPercent: -350 })
-  // .set("#animation1", { opacity: 0 })
-  .set("#firstWord", { opacity: 0 })
-  .set("#secondWord", { opacity: 0 })
-  .set("#thirdWord", { opacity: 0 })
-  .set("#fourthWord", { opacity: 0 })
-  .set("#fifthWord", { opacity: 0 })
-  .set("#sixthWord", { opacity: 0 })
-  .set("#seventhWord", { opacity: 0 })
-  .set("#eightWord", { opacity: 0 })
-  .set("#ninthWord", { opacity: 0 })
-  .set("#tenthWord", { opacity: 0 })
-  .set("#eleventhWord", { opacity: 0 })
 
   // SET LAPTOP CENTER
   .to(".videoSectionFull", {
-    scale: 0.6,
-    ease: "none",
+    ease: "slow",
     yPercent: 15,
     transformOrigin: "center top",
   })
 
-  // SET TEXT TO COME
-  .to("#firstWord", 0, {
-    ease: "slow",
-    y: -750,
-    xPercent: 5,
-    // x: () => window.innerWidth - 0,
-    opacity: 1,
-  })
-  // SET LAPTOP TO RIGHT AFTER CENTER
-  .to("#animation1", { opacity: 1 })
-  .to(".videoSectionFull", {
-    scale: 0.6,
-    // ease: "none",
-    x: 200,
-    transformOrigin: "center top",
-  })
-  .to(".videoPlay", { opacity: 0 })
-  .to("#secondWord", 1, { ease: "slow", y: -750, xPercent: 5, opacity: 1 })
-  .to("#thirdWord", 2, { ease: "slow", y: -750, xPercent: 5, opacity: 1 })
+// // Set Animation Gone
+// .set("#animation3", { xPercent: 350 })
+// .set("#animation2", { xPercent: -350 })
+// // .set("#animation1", { opacity: 0 })
+// .set("#firstWord", { opacity: 0 })
+// .set("#secondWord", { opacity: 0 })
+// .set("#thirdWord", { opacity: 0 })
+// .set("#fourthWord", { opacity: 0 })
+// .set("#fifthWord", { opacity: 0 })
+// .set("#sixthWord", { opacity: 0 })
+// .set("#seventhWord", { opacity: 0 })
+// .set("#eightWord", { opacity: 0 })
+// .set("#ninthWord", { opacity: 0 })
+// .set("#tenthWord", { opacity: 0 })
+// .set("#eleventhWord", { opacity: 0 })
 
-  // FADE AWAY MOMENT
-  .to("#firstWord", 1, { ease: "slow", opacity: 0 })
-  .to("#fourthWord", 0, { ease: "slow", y: -1050, opacity: 1 })
-  .to("#animation1", 1, {
-    duration: 20,
-    ease: "slow(0.2, 0.2, false)",
-    xPercent: -500,
-  })
-  .to("#secondWord", 1, { ease: "power1", opacity: 0 })
-  .to("#thirdWord", 1, { ease: "power1", opacity: 0 })
-  .to("#animation2", 1, {
-    duration: 10,
-    ease: "power1",
-    xPercent: -90,
-  })
-  .to("#fourthWord", 1, { ease: "power1", opacity: 0 })
-  .to("#fifthWord", 1, {
-    ease: "slow",
-    y: -1050,
-    xPercent: 70,
-    opacity: 1,
-  })
-  .to("#sixthWord", 1, { ease: "slow", y: -1050, xPercent: 70, opacity: 1 })
-  .to("#seventhWord", 1, { ease: "slow", y: -1000, xPercent: 70, opacity: 1 })
-  .to("#animation2", 1, { ease: "power1", xPercent: 500 })
-  .to("#fifthWord", 1, { ease: "power1", opacity: 0 })
-  .to("#eightWord", 1, { ease: "power1", y: -1250, opacity: 1 })
-  .to("#sixthWord", 1, { ease: "power1", opacity: 0 })
-  .to("#animation3", 1, {
-    duration: 1,
-    ease: "power1",
-    xPercent: 0,
-  })
-  .to("#seventhWord", 1, { ease: "power1", opacity: 0 })
-  .to("#eightWord", 1, { ease: "power1", opacity: 0 })
-  .to("#ninthWord", 1, { ease: "slow", y: -1450, xPercent: 10, opacity: 1 })
-  .to("#tenthWord", 1, { ease: "slow", y: -1450, xPercent: 10, opacity: 1 })
-  .to("#eleventhWord", 1, {
-    ease: "slow",
-    y: -1450,
-    xPercent: 10,
-    opacity: 1,
-  })
-  .to("#animation3", 1, {
-    duration: 1,
-    ease: "power1",
-    xPercent: -500,
-  })
-  .to("#ninthWord", 1, { ease: "power1", opacity: 0 })
-  .to("#tenthWord", 1, { ease: "power1", opacity: 0 })
-  .to("#eleventhWord", 1, { ease: "power1", opacity: 0 });
+// // SET LAPTOP CENTER
+// .to(".videoSectionFull", {
+//   scale: 0.6,
+//   ease: "none",
+//   yPercent: 15,
+//   transformOrigin: "center top",
+// })
+
+// // SET TEXT TO COME
+// .to("#firstWord", 0, {
+//   ease: "slow",
+//   y: -750,
+//   xPercent: 5,
+//   // x: () => window.innerWidth - 0,
+//   opacity: 1,
+// })
+// // SET LAPTOP TO RIGHT AFTER CENTER
+// .to("#animation1", { opacity: 1 })
+// .to(".videoSectionFull", {
+//   scale: 0.6,
+//   // ease: "none",
+//   x: 200,
+//   transformOrigin: "center top",
+// })
+// .to(".videoPlay", { opacity: 0 })
+// .to("#secondWord", 1, { ease: "slow", y: -750, xPercent: 5, opacity: 1 })
+// .to("#thirdWord", 2, { ease: "slow", y: -750, xPercent: 5, opacity: 1 })
+
+// // FADE AWAY MOMENT
+// .to("#firstWord", 1, { ease: "slow", opacity: 0 })
+// .to("#fourthWord", 0, { ease: "slow", y: -1050, opacity: 1 })
+// .to("#animation1", 1, {
+//   duration: 20,
+//   ease: "slow(0.2, 0.2, false)",
+//   xPercent: -500,
+// })
+// .to("#secondWord", 1, { ease: "power1", opacity: 0 })
+// .to("#thirdWord", 1, { ease: "power1", opacity: 0 })
+// .to("#animation2", 1, {
+//   duration: 10,
+//   ease: "power1",
+//   xPercent: -90,
+// })
+// .to("#fourthWord", 1, { ease: "power1", opacity: 0 })
+// .to("#fifthWord", 1, {
+//   ease: "slow",
+//   y: -1050,
+//   xPercent: 70,
+//   opacity: 1,
+// })
+// .to("#sixthWord", 1, { ease: "slow", y: -1050, xPercent: 70, opacity: 1 })
+// .to("#seventhWord", 1, { ease: "slow", y: -1000, xPercent: 70, opacity: 1 })
+// .to("#animation2", 1, { ease: "power1", xPercent: 500 })
+// .to("#fifthWord", 1, { ease: "power1", opacity: 0 })
+// .to("#eightWord", 1, { ease: "power1", y: -1250, opacity: 1 })
+// .to("#sixthWord", 1, { ease: "power1", opacity: 0 })
+// .to("#animation3", 1, {
+//   duration: 1,
+//   ease: "power1",
+//   xPercent: 0,
+// })
+// .to("#seventhWord", 1, { ease: "power1", opacity: 0 })
+// .to("#eightWord", 1, { ease: "power1", opacity: 0 })
+// .to("#ninthWord", 1, { ease: "slow", y: -1450, xPercent: 10, opacity: 1 })
+// .to("#tenthWord", 1, { ease: "slow", y: -1450, xPercent: 10, opacity: 1 })
+// .to("#eleventhWord", 1, {
+//   ease: "slow",
+//   y: -1450,
+//   xPercent: 10,
+//   opacity: 1,
+// })
+// .to("#animation3", 1, {
+//   duration: 1,
+//   ease: "power1",
+//   xPercent: -500,
+// })
+// .to("#ninthWord", 1, { ease: "power1", opacity: 0 })
+// .to("#tenthWord", 1, { ease: "power1", opacity: 0 })
+// .to("#eleventhWord", 1, { ease: "power1", opacity: 0 });
 
 // LOAD CANVAS ROTATION 1
 const canvas = document.getElementById("animation1");
@@ -168,100 +170,100 @@ function render() {
   // console.clear();
 }
 
-// END LOAD CANVAS ROTATION 1
+// // END LOAD CANVAS ROTATION 1
 
-// LOAD CANVAS ROTATION 2
-const canvas2 = document.getElementById("animation2");
-const context2 = canvas2.getContext("2d");
+// // LOAD CANVAS ROTATION 2
+// const canvas2 = document.getElementById("animation2");
+// const context2 = canvas2.getContext("2d");
 
-canvas2.width = 1440;
-canvas2.height = 1440;
+// canvas2.width = 1440;
+// canvas2.height = 1440;
 
-const frameCount2 = 47;
-// const currentFrame = (index) =>
-//   `https://laravel3.isysedge.com/gsap_try/assets/images/${(
-//     index + 1
-//   ).toString()}.png`;
-const currentFrame2 = (index) =>
-  `assets/images/yoga/${(index + 1).toString()}.png`;
+// const frameCount2 = 47;
+// // const currentFrame = (index) =>
+// //   `https://laravel3.isysedge.com/gsap_try/assets/images/${(
+// //     index + 1
+// //   ).toString()}.png`;
+// const currentFrame2 = (index) =>
+//   `assets/images/yoga/${(index + 1).toString()}.png`;
 
-const images2 = [];
-const laptops2 = {
-  frame: 0,
-};
+// const images2 = [];
+// const laptops2 = {
+//   frame: 0,
+// };
 
-for (let i = 0; i < frameCount2; i++) {
-  const img = new Image();
-  if (i >= 0) {
-    img.src = currentFrame2(i);
-    images2.push(img);
-  }
-  // console.log(img.src);
-}
+// for (let i = 0; i < frameCount2; i++) {
+//   const img = new Image();
+//   if (i >= 0) {
+//     img.src = currentFrame2(i);
+//     images2.push(img);
+//   }
+//   // console.log(img.src);
+// }
 
-tl.to(laptops2, {
-  frame: frameCount2 - 1,
-  snap: "frame",
-  ease: "none",
-  scrollTrigger: {
-    trigger: ".animate",
-    scrub: 0.5,
-  },
-  onUpdate: render2, // use animation onUpdate instead of scrollTrigger's onUpdate
-});
+// tl.to(laptops2, {
+//   frame: frameCount2 - 1,
+//   snap: "frame",
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: ".animate",
+//     scrub: 0.5,
+//   },
+//   onUpdate: render2, // use animation onUpdate instead of scrollTrigger's onUpdate
+// });
 
-images2[0].onload = render;
+// images2[0].onload = render;
 
-function render2() {
-  context2.clearRect(0, 0, canvas2.width, canvas2.height);
-  context2.drawImage(images2[laptops2.frame], 0, 0);
-  // console.clear();
-}
+// function render2() {
+//   context2.clearRect(0, 0, canvas2.width, canvas2.height);
+//   context2.drawImage(images2[laptops2.frame], 0, 0);
+//   // console.clear();
+// }
 
-// END LOAD CANVAS ROTATION 2
+// // END LOAD CANVAS ROTATION 2
 
-// LOAD CANVAS ROTATION 3
-const canvas3 = document.getElementById("animation3");
-const context3 = canvas3.getContext("2d");
+// // LOAD CANVAS ROTATION 3
+// const canvas3 = document.getElementById("animation3");
+// const context3 = canvas3.getContext("2d");
 
-canvas3.width = 1440;
-canvas3.height = 1440;
+// canvas3.width = 1440;
+// canvas3.height = 1440;
 
-const frameCount3 = 77;
-const currentFrame3 = (index) =>
-  `assets/images/5ipro/${(index + 1).toString()}.png`;
+// const frameCount3 = 77;
+// const currentFrame3 = (index) =>
+//   `assets/images/5ipro/${(index + 1).toString()}.png`;
 
-const images3 = [];
-const laptops3 = {
-  frame: 0,
-};
+// const images3 = [];
+// const laptops3 = {
+//   frame: 0,
+// };
 
-for (let i = 0; i < frameCount3; i++) {
-  const img = new Image();
-  if (i >= 0) {
-    img.src = currentFrame3(i);
-    images3.push(img);
-  }
-  // console.log(img.src);
-}
+// for (let i = 0; i < frameCount3; i++) {
+//   const img = new Image();
+//   if (i >= 0) {
+//     img.src = currentFrame3(i);
+//     images3.push(img);
+//   }
+//   // console.log(img.src);
+// }
 
-tl.to(laptops3, {
-  frame: frameCount3 - 1,
-  snap: "frame",
-  ease: "none",
-  scrollTrigger: {
-    trigger: ".animate",
-    scrub: 0.5,
-  },
-  onUpdate: render3, // use animation onUpdate instead of scrollTrigger's onUpdate
-});
+// tl.to(laptops3, {
+//   frame: frameCount3 - 1,
+//   snap: "frame",
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: ".animate",
+//     scrub: 0.5,
+//   },
+//   onUpdate: render3, // use animation onUpdate instead of scrollTrigger's onUpdate
+// });
 
-images3[0].onload = render;
+// images3[0].onload = render;
 
-function render3() {
-  context3.clearRect(0, 0, canvas3.width, canvas3.height);
-  context3.drawImage(images3[laptops3.frame], 0, 0);
-  // console.clear();
-}
+// function render3() {
+//   context3.clearRect(0, 0, canvas3.width, canvas3.height);
+//   context3.drawImage(images3[laptops3.frame], 0, 0);
+//   // console.clear();
+// }
 
-// END LOAD CANVAS ROTATION 3
+// // END LOAD CANVAS ROTATION 3
